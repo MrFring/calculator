d4436e02c4be986644f32f832b494fb135d2d260
MrFring <mrzhangchi@outlook.com>
2020-06-30 22:13:19 +0800

    MyCal

---
+++ MyCal2/MyCal2/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace MyCal2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.textBoxOp1 = new System.Windows.Forms.TextBox();
+            this.textBoxOp2 = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonEqual = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelResult);
+            this.panel1.Location = new System.Drawing.Point(28, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1242, 153);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBoxOp2);
+            this.panel2.Controls.Add(this.textBoxOp1);
+            this.panel2.Location = new System.Drawing.Point(28, 225);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(511, 454);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.buttonEqual);
+            this.panel3.Controls.Add(this.buttonDiv);
+            this.panel3.Controls.Add(this.buttonMul);
+            this.panel3.Controls.Add(this.buttonSub);
+            this.panel3.Controls.Add(this.buttonAdd);
+            this.panel3.Location = new System.Drawing.Point(601, 225);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(669, 454);
+            this.panel3.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(524, 15);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 73);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSub.Location = new System.Drawing.Point(524, 115);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(75, 73);
+            this.buttonSub.TabIndex = 1;
+            this.buttonSub.Text = "-";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonSub_Click);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMul.Location = new System.Drawing.Point(524, 220);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(75, 73);
+            this.buttonMul.TabIndex = 2;
+            this.buttonMul.Text = "×";
+            this.buttonMul.UseVisualStyleBackColor = true;
+            this.buttonMul.Click += new System.EventHandler(this.buttonMul_Click);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDiv.Location = new System.Drawing.Point(524, 328);
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.Size = new System.Drawing.Size(75, 73);
+            this.buttonDiv.TabIndex = 3;
+            this.buttonDiv.Text = "÷";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.buttonDiv_Click);
+            // 
+            // textBoxOp1
+            // 
+            this.textBoxOp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOp1.Location = new System.Drawing.Point(44, 50);
+            this.textBoxOp1.Name = "textBoxOp1";
+            this.textBoxOp1.Size = new System.Drawing.Size(398, 38);
+            this.textBoxOp1.TabIndex = 0;
+            // 
+            // textBoxOp2
+            // 
+            this.textBoxOp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOp2.Location = new System.Drawing.Point(44, 133);
+            this.textBoxOp2.Name = "textBoxOp2";
+            this.textBoxOp2.Size = new System.Drawing.Size(398, 38);
+            this.textBoxOp2.TabIndex = 1;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(41, 51);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(92, 31);
+            this.labelResult.TabIndex = 0;
+            this.labelResult.Text = "Result";
+            // 
+            // buttonEqual
+            // 
+            this.buttonEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEqual.Location = new System.Drawing.Point(377, 328);
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.Size = new System.Drawing.Size(75, 73);
+            this.buttonEqual.TabIndex = 4;
+            this.buttonEqual.Text = "=";
+            this.buttonEqual.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1307, 719);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxOp2;
+        private System.Windows.Forms.TextBox textBoxOp1;
+        private System.Windows.Forms.Button buttonEqual;
+    }
+}
+
---
+++ MyCal2/MyCal2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCal2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x + y;
+            this.labelResult.Text = z.ToString();
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x - y;
+            this.labelResult.Text = z.ToString();
+        }
+
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x * y;
+            this.labelResult.Text = z.ToString();
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            if (y != 0) {
+                var z = x / y;
+                this.labelResult.Text = z.ToString();
+            }            
+        }
+    }
+}
